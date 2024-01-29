f4aba249699c2300307f639af8cbe83073b42b46
Thomas <minhyoungan@Thomas.local>
2024-01-29 15:51:50 +0900

    first commit

---
+++ exer2.py
@@ -0,0 +1,9 @@
+input_name = input('Please input your name : ')
+input_years = input('Please input your years : ')
+input_sex = input('Please input your sex : ')
+input_bloodtype = input('Please input your blood type : ')
+print('your name is', input_name)
+print('your years is', input_years)
+print('your sex is', input_sex)
+print('your blood type is', input_bloodtype)
+
---
+++ exer3.py
@@ -0,0 +1,5 @@
+sum =0
+print('Now sum is  ', sum)
+
+get_num1 = input('Please input to integer : ')
+sum = sum + int(get_num1)

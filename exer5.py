ffedd0b2812f7b9ac4b3209e109bf6b7e98f375a
amin0727 <ymsamh@icloud.com>
2024-01-30 15:54:56 +0900

    modified:   exer5.py

--- exer5.py
+++ exer5.py
@@ -32,12 +32,54 @@ step = 2
 for count in range (start, stop, step) : # start ~ stop +step > count
     print('for : ' ,count)
 
-'''
+
 sum =0
+count = 1
 for count in range(5):
+    print(count+1, end='')
     nums = input('st input years : ')
-    print(count, nums)
     sum += int(nums)
     
 print(sum)
 
+'''
+
+#ここから課題１：Whileを使ってfactorial計算
+#課題２：for文を使ってfactorial計算
+'''
+print('factorialを計算します ',end = '')
+base = int(input('基数を入力してください：'))
+result = 1
+count = 1
+num = base
+
+while count < base :
+    
+    result *= num
+    num -= 1
+    count += 1
+    
+if base >= 2 :
+    print(base,'のfactorialは',result,'です')
+elif base == 0 or base == 1:
+    print(base,'のfactorialは1です')
+elif base < 0 :
+    print('0以上の整数を入力してください')
+    
+'''
+print('factorialを計算します ',end = '')
+base = int(input('基数を入力してください：'))
+result = 1
+count = 1
+
+
+for count in range(1, base+1, 1) :
+    result *= count
+    
+if base >= 2 :
+    print(base,'のfactorialは',result,'です')
+elif base == 0 or base == 1:
+    print(base,'のfactorialは1です')
+elif base < 0 :
+    print('0以上の整数を入力してください')
+    
